d25c3288b254515c138b68d546402b40cd1749ab
giovaniv <giovaniv@gmail.com>
2018-06-07 02:08:29 +0000

    first commit

---
+++ sales-tax/sales-tax.js
@@ -0,0 +1,73 @@
+var salesTaxRates = {
+  AB: 0.05,
+  BC: 0.12,
+  SK: 0.10
+};
+
+var companySalesData = [
+  {
+    name: "Telus",
+    province: "BC",
+    sales: [ 100, 200, 400 ]
+  },
+  {
+    name: "Bombardier",
+    province: "AB",
+    sales: [ 80, 20, 10, 100, 90, 500 ]
+  },
+  {
+    name: "Telus",
+    province: "SK",
+    sales: [ 500, 100 ]
+  }
+];
+
+function addCompanySalesTotal(sales) {
+  var total = 0;
+  for (var i = 0; i < sales.length; i++) {
+    total += sales[i];
+  }
+  return total;
+}
+
+function addCompanyTaxes(total, province) {
+  return total * salesTaxRates[province];
+}
+
+function calculateSalesTax(salesData, taxRates) {
+
+  var result = {};
+
+  // var companyName = salesData[1].name;
+  // var totalSales = addCompanySalesTotal(salesData[1].sales);
+  // var totalTaxes = addCompanyTaxes(totalSales, salesData[1].province);
+  // result[companyName] = { totalSales: totalSales, totalTaxes: totalTaxes };
+  // console.log(result);
+
+  for (var i = 0; i < salesData.length; i++) {
+
+    var companyName = salesData[i].name;
+    var totalSales = addCompanySalesTotal(salesData[i].sales);
+    var totalTaxes = addCompanyTaxes(totalSales, salesData[i].province);
+    result[companyName] = { totalSales: totalSales, totalTaxes: totalTaxes };
+
+  }
+
+  console.log(result);
+
+}
+
+var results = calculateSalesTax(companySalesData, salesTaxRates);
+
+/* Expected Results:
+{
+  Telus: {
+    totalSales: 1300
+    totalTaxes: 144
+  },
+  Bombardier: {
+    totalSales: 800,
+    totalTaxes: 40
+  }
+}
+*/
